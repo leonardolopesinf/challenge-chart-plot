6d19aeeb6cd7c727c288921f5d1539145b9c17d3
Leonardo Lopes <leonardolopesinf@gmail.com>
2022-01-28 10:31:15 -0300

    feat: add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+    colors: {
+        
+    },
+    codeEditor: {
+        default: '#ffffff',
+        variable: '#ffffff',
+        string: '#0DCBDC',
+        number: '#A376CD',
+        lineNumber: '#3E434E',
+
+        background: '#2E3440',
+        guttersBackground: '#60656E',
+
+        scrollbar: '#82858D',
+    }
+}
+
+export default theme;
