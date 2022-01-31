84220c7d61e9ef8f03725533d0b79ebadfb00c53
Leonardo Lopes <leonardolopesinf@gmail.com>
2022-01-31 01:58:39 -0300

    feat: add new colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,19 +1,23 @@
 const theme = {
-    colors: {
-        
-    },
-    codeEditor: {
-        default: '#ffffff',
-        variable: '#ffffff',
-        string: '#0DCBDC',
-        number: '#A376CD',
-        lineNumber: '#3E434E',
+  colors: {
+    dark: '#2E3440',
+    light: '#ffffff',
+    highlight: '#0180FF',
 
-        background: '#2E3440',
-        guttersBackground: '#60656E',
+    headerBackground: '#DDDEE1',
+  },
+  codeEditor: {
+    default: '#ffffff',
+    variable: '#ffffff',
+    string: '#0DCBDC',
+    number: '#A376CD',
+    lineNumber: '#3E434E',
 
-        scrollbar: '#82858D',
-    }
-}
+    background: '#2E3440',
+    guttersBackground: '#60656E',
+
+    scrollbar: '#82858D',
+  },
+};
 
 export default theme;
